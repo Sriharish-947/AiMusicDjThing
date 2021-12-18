6272f88229250152d8542ec5ece85dfe40a1d281
Sriharish-947 <74801176+Sriharish-947@users.noreply.github.com>
2021-12-17 19:46:59 -0500

    Add files via upload

---
+++ AI_Music_web_App/main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+/* Html code that I got rid of that I might use eventually 
+</h3>
+    <div class="disk_speed">
+      <img src="disk.gif" class="div_speed_img"> 
+      <br>
+      <h3 class="btn btn-danger" id="speed"> Speed </h3>
+    </div>
+
+    <div class="disk_volume">
+      <img src="disk.gif" class="div_volume_img"> 
+      <br>
+      <h3 class="btn btn-danger" id="volume"> volume </h3>
+    </div>
+	<br><br>
+*/
